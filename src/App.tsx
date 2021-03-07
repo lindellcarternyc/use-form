ec1e29392ebd27440b002c80e0e1d4c87b24b1db
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-07 15:04:19 -0500

    add examples to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import { CheckboxGroupExample, RadioInputExample } from './use-form/Input.example'
-// import { NumberInputExample } from './use-form/Input.example'
-
+import { 
+  CheckboxGroupExample, 
+  NumberInputExample, 
+  RadioInputExample,
+  TextInputExample
+} from './use-form/Input.example'
 function App() {
-  const [state, setState] = useState({
-    checkbox: false
-  })
-
-  const onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    if (evt.target.type === 'checkbox') {
-      setState({
-        ...state,
-        [evt.target.name]: evt.target.checked
-      })
-    }
-  }
 
   return (
     <div className="App">
-      {/* <NumberInputExample /> */}
-      {/* <input 
-        name="checkbox"
-        type="checkbox"
-        onChange={onChange}
-        checked={state.checkbox}
-      />
-      <CheckboxGroupExample /> */}
+      <NumberInputExample /> 
+      <br />
+      <CheckboxGroupExample />
+      <br />
       <RadioInputExample />
+      <br />
+      <TextInputExample />
     </div>
   );
 }
